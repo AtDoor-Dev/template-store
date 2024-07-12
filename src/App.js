b8bd7d6904d3b848ec26b97dfbf85718cfa909af
annisha-atdoor <annisha@atdoor.co.in>
2024-07-12 18:27:29 +0530

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import DataTable from './Horoscope';
+import React, { useState } from 'react';
+import DataTable from './Horoscope'; 
 import './App.css';
 
 function App() {
+  const [selectedGender, setSelectedGender] = useState('male');
+
+  const handleGenderChange = (e) => {
+    setSelectedGender(e.target.value);
+  };
+
   return (
     <div className="App">
-          {/* <img src={images} alt="Canvas Logo" width="2%" className="logo-image" /> */}
-      <DataTable />
-      {/* <Horoscope data={data} /> */}
+      <div className="gender-dropdown">
+        <label htmlFor="gender-select">Select Gender: </label>
+        <select id="gender-select" value={selectedGender} onChange={handleGenderChange}>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+      </div>
+      <DataTable selectedGender={selectedGender} />
     </div>
   );
 }
--- src/Horoscope.js
+++ src/Horoscope.js
@@ -23,11 +23,10 @@ const formatHeight = (height) => {
   return `${feet}'${inches}"`;
 };
 
-
 const Horoscope = ({ imageSrc }) => {
   return (
     <div className="horoscope-container">
-      <table className="horoscope-table" style={{ width: '34%'  }}>
+      <table className="horoscope-table" style={{ width: '34%' }}>
         <tbody>
           {table1.map((row, rowIndex) => (
             <tr key={rowIndex}>
@@ -40,7 +39,7 @@ const Horoscope = ({ imageSrc }) => {
           ))}
         </tbody>
       </table>
-        <img src={imageBetweenTables} alt="Image Between Tables" />
+      <img src={imageBetweenTables} alt="Image Between Tables" />
       <table className="horoscope-table" style={{ width: '33%' }}>
         <tbody>
           {table2.map((row, rowIndex) => (
@@ -58,117 +57,114 @@ const Horoscope = ({ imageSrc }) => {
   );
 };
 
-const DataTable = () => {
+const DataTable = ({ selectedGender }) => {
   const { birthDetails, horoscopeDetails, familyDetails, contactDetails, personalInfo, ownHouse, qualification, occupation, residentialAddress } = data;
 
   return (
-    
     <div className="a4">
-     <div className="background-image">
-      {/* <img src={imageEnabled} alt=" image-Enabled"  className="enable-image" /> */}
-      <div className="main-header">
-        <div className="top-heading">
-          <div className="heading">
-          <img src={images} alt="Canvas Logo"  className="logo-image" />
-            Vasavi Sahayam <br /> Angala Parameshwari Amman Thunai
+      <div className="background-image">
+        <div className="main-header">
+          <div className="top-heading">
+            <div className="heading">
+              <img src={images} alt="Canvas Logo" className="logo-image" />
+              Vasavi Sahayam <br /> Angala Parameshwari Amman Thunai
+            </div>
+            <div className="sub-heading">
+              Janani Janma Sowkyanam Varathanee Kulasumbathaam<br />
+              Padhavi Pooorva Punyanaam Likyathe Janma Pathrika
+            </div>
           </div>
-          <div className="sub-heading">
-            Janani Janma Sowkyanam Varathanee Kulasumbathaam<br />
-            Padhavi Pooorva Punyanaam Likyathe Janma Pathrika
+          <div className="personal-details">
+            <table>
+              <tbody className="table">
+                <tr>
+                  <th scope="row">Date of Birth</th>
+                  <td>{birthDetails.dob.dateOfBirth}</td>
+                  <th scope="row">Naaligai</th>
+                  <td>{horoscopeDetails.naaligai}</td>
+                </tr>
+                <tr>
+                  <th scope="row">TamilYear</th>
+                  <td>{birthDetails.tamilYear}</td>
+                  <th scope="row">Birth Star</th>
+                  <td>{horoscopeDetails.BirthStar}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Tamil Month</th>
+                  <td>{birthDetails.tamilMonth}</td>
+                  <th scope="row">Rasi</th>
+                  <td>{horoscopeDetails.rasi}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Tamil Date</th>
+                  <td>{birthDetails.tamilDate}</td>
+                  <th scope="row">Thithi</th>
+                  <td>{horoscopeDetails.thithi}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Day</th>
+                  <td>{birthDetails.dob.day}</td>
+                  <th scope="row">Lagnam</th>
+                  <td>{horoscopeDetails.lagnam}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Birth Time</th>
+                  <td>{birthDetails.dob.time}</td>
+                  <th scope="row">Place Of Birth</th>
+                  <td>{birthDetails.dob.placeOfBirth}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="below-heading">
+            S/o Late <b> Mr. Ponmani - Mrs. Porselvi</b>
+          </div>
+          <div className="second-name">
+            <p className="name">{selectedGender === 'male' ? 'Kumaran' : 'Kumari'}.<b> Kamalesh P</b></p>
+          </div>
+          <div className="third-line">
+            <h5>Gothram : Shiva Gothram &nbsp;&nbsp; Madhulam : dfdfdsa</h5>
           </div>
         </div>
-        <div className="personal-details">
-          <table>
-            <tbody className="table">
+        <Horoscope imageSrc="path_to_image" />
+        <div className="mid-heading">
+          <h3><span className="blue-text">Mars </span> <span className="brown-text">Dasa Balance : 6months</span></h3>
+        </div>
+        <div className="family-details">
+          <table className="family-table">
+            <tbody>
               <tr>
-                <th scope="row">Date of Birth</th>
-                <td>{birthDetails.dob.dateOfBirth}</td>
-                <th scope="row">Naaligai</th>
-                <td>{horoscopeDetails.naaligai}</td>
+                <td>
+                  <b>Father</b> : {familyDetails.fatherAlive} - <b>Mother</b> : {familyDetails.motherAlive}
+                </td>
+                <td><b>Height</b>: {formatHeight(familyDetails.height)}</td>
+                <td><b>Blood Group</b> : {personalInfo.bloodGroup}</td>
               </tr>
               <tr>
-                <th scope="row">TamilYear</th>
-                <td>{birthDetails.tamilYear}</td>
-                <th scope="row">Birth Star</th>
-                <td>{horoscopeDetails.BirthStar}</td>
+                <td>
+                  <b>Brother</b> : {familyDetails.siblings.brother.number} (Elder-{familyDetails.siblings.brother.marriageNum})
+                </td>
+                <td><b>Complextion</b> : {personalInfo.complextion}</td>
+                <td><b>OwnHouse</b> : {ownHouse}</td>
               </tr>
               <tr>
-                <th scope="row">Tamil Month</th>
-                <td>{birthDetails.tamilMonth}</td>
-                <th scope="row">Rasi</th>
-                <td>{horoscopeDetails.rasi}</td>
-              </tr>
-              <tr>
-                <th scope="row">Tamil Date</th>
-                <td>{birthDetails.tamilDate}</td>
-                <th scope="row">Thithi</th>
-                <td>{horoscopeDetails.thithi}</td>
-              </tr>
-              <tr>
-                <th scope="row">Day</th>
-                <td>{birthDetails.dob.day}</td>
-                <th scope="row">Lagnam</th>
-                <td>{horoscopeDetails.lagnam}</td>
-              </tr>
-              <tr>
-                <th scope="row">Birth Time</th>
-                <td>{birthDetails.dob.time}</td>
-                <th scope="row">Place Of Birth</th>
-                <td>{birthDetails.dob.placeOfBirth}</td>
+                <td><b>Education</b>: {qualification.Education}</td>
+                <td><b>Profession</b> : {personalInfo.Profession}</td>
+                <td><b>Income</b> : {occupation.Income}</td>
               </tr>
             </tbody>
           </table>
-         </div>
-          <div className="below-heading">
-           S/o Late   <b> Mr. Ponmani -   Mrs. Porselvi</b>
-          </div>
-          <div className="second-name">
-            <p class="name">Chiranjeevi.<b> Kamalesh P</b></p>
-          </div>
-          <div className="third-line">
-           <h5> Gothram : Shiva Gothram &nbsp;&nbsp; Madhulam : dfdfdsa</h5>
-          </div>
         </div>
-      <Horoscope imageSrc="path_to_image" />
-      <div className="mid-heading">
-      <h3><span className="blue-text">Mars </span> <span className="brown-text">Dasa Balance : 6months</span></h3>
+        <div className="residential-address">
+          <p className="address">Address: {residentialAddress.address} {residentialAddress.landmark} {residentialAddress.hometown}, {residentialAddress.city}, {residentialAddress.stateName} {residentialAddress.pincode} {residentialAddress.nationality}</p>
+        </div>
+        <div className="contact-details">
+          <p className="address">Mobile : {contactDetails.contactNumber} &nbsp;&nbsp; &nbsp;&nbsp; WhatsApp Number : {contactDetails.whatsappNumber}</p>
+        </div>
+        <div className="email-address">
+          <p className="address">Email: {contactDetails.emailId}</p>
+        </div>
       </div>
-      <div className="family-details">
-          <table className="family-table">
-          <tbody>
-          <tr>
-            <td>
-              <b>Father</b> : {familyDetails.fatherAlive} - <b>Mother</b> : {familyDetails.motherAlive}
-            </td>
-            <td><b>Height</b>: {formatHeight(familyDetails.height)}</td>
-            <td><b>Blood Group</b> : {personalInfo.bloodGroup}</td>
-          </tr>
-          <tr>
-            <td>
-             <b> Brother</b> : {familyDetails.siblings.brother.number}  (Elder-{familyDetails.siblings.brother.marriageNum})
-            </td>
-            <td><b>Complextion</b> : {personalInfo.complextion}</td>
-            <td><b>OwnHouse</b> : {ownHouse}</td>
-          </tr>
-          <tr>
-            <td><b>Education</b>: {qualification.Education}</td>
-            <td><b>Profession</b> : {personalInfo.Profession}</td>
-            <td><b>Income</b> : {occupation.Income}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div className="residential-address">
-          <p class="address"> Address:{residentialAddress.address}  {residentialAddress.landmark} {residentialAddress.hometown}, {residentialAddress.city}, {residentialAddress.stateName} {residentialAddress.pincode}  {residentialAddress.nationality}</p>
-
-    </div>
-    <div className="contact-details">
-          <p class="address">Mobile : {contactDetails.contactNumber}  &nbsp;&nbsp; &nbsp;&nbsp;  WhatsApp Number : {contactDetails.whatsappNumber}</p>
-        </div>
-        <div class="email-address"> 
-        <p class="address">Email: {contactDetails.emailId}</p>
-        </div>
-    </div>
     </div>
   );
 };
